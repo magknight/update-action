92c0476d529a33ed48aca1f058bb53fce940f5ac
Alexander Beavil <alex@aeroniemi.com>
2022-04-14 23:07:02 +0100

    it actually works (with zero error handling)

--- index.js
+++ index.js
@@ -2,17 +2,18 @@ const core = require("@actions/core");
 const fs = require("fs");
 const path = require("path");
 const ftp = require("basic-ftp");
+const { match } = require("assert");
 var version = core.getInput("version", { required: true });
 var username = core.getInput("username", { required: true });
 var password = core.getInput("password", { required: true });
 var ftpUrl = core.getInput("ftpUrl", { required: true });
+var fileName = "updaterListing.json";
 core.setSecret("username");
 core.setSecret("password");
+const client = new ftp.Client();
+client.ftp.verbose = true;
 async function download() {
 	core.info("Beginning download setup");
-	// return true;
-	const client = new ftp.Client();
-	client.ftp.verbose = true;
 	try {
 		await client.access({
 			host: ftpUrl,
@@ -21,21 +22,74 @@ async function download() {
 			secure: true,
 		});
 		core.info("Downloading");
-		await client.downloadTo("updaterListing.json", "updaterListing.json");
+		await client.downloadTo(fileName, fileName);
 	} catch (err) {
-		console.log(err);
+		core.error(err);
 	}
 	core.info("Download complete");
 	client.close();
 }
-async function edit() {
-	var file = await fs.readFileAsync("./updaterListing.json");
+async function upload() {
+	core.info("Beginning download setup");
+	try {
+		await client.access({
+			host: ftpUrl,
+			user: username,
+			password: password,
+			secure: true,
+		});
+		core.info("Uploading ");
+		await client.uploadFrom(`output-${fileName}`, fileName);
+	} catch (err) {
+		core.error(err);
+	}
+	core.info("Download complete");
+	client.close();
+}
+
+async function identifyBranch() {
+	if (!/v\d+.\d+.\d+/.test(version)) {
+		throw new Error(
+			`Version string does not fit required format: '${version}'`
+		);
+	}
+	if (/alpha/.test(version)) {
+		return "aviatorsEditionCygnus", "alpha";
+	} else if (/beta/.test(version)) {
+		return "aviatorsEditionCygnus", "beta";
+	} else if (/libra/.test(version)) {
+		return "aviatorsEditionLibra", "libra";
+	} else if (!/-/.test(version)) {
+		return "aviatorsEditionLive", "stable";
+	} else if (/releaseCandidate/.test(version)) {
+		return "aviatorsEditionLive", "releaseCandidate";
+	}
+	throw new Error("Branch unknown");
+}
+
+async function edit(branch, branchType) {
+	core.info(`Reading File ${fileName}`);
+	var file = fs.readFileSync(fileName);
 	var content = JSON.parse(file);
 	core.info(content);
+	content[branch] = {
+		name: branch,
+		type: branchType,
+		version: version,
+		dateTime: Math.floor(Date.now() / 1000),
+		enabled: true,
+	};
+	fs.writeFileSync(`output-${fileName}`, JSON.stringify(content));
+	core.info("Written");
+	return;
 }
 
 async function run() {
+	let branch,
+		branchType = await identifyBranch();
+	core.info(`Branch is '${branch}'; type is: '${branchType}'`);
 	await download();
-	await edit();
+	await edit(branch, branchType);
+	await upload();
 }
 run();
