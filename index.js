e3ca216ceb49ce08acb06ab62833d5c694873168
Alexander Beavil <alex@aeroniemi.com>
2022-04-14 23:12:51 +0100

    basic error handling

--- index.js
+++ index.js
@@ -25,9 +25,11 @@ async function download() {
 		await client.downloadTo(fileName, fileName);
 	} catch (err) {
 		core.error(err);
+	} finally {
+		core.info("Download complete");
+		client.close();
+		return;
 	}
-	core.info("Download complete");
-	client.close();
 }
 async function upload() {
 	core.info("Beginning download setup");
@@ -42,54 +44,69 @@ async function upload() {
 		await client.uploadFrom(`output-${fileName}`, fileName);
 	} catch (err) {
 		core.error(err);
+	} finally {
+		core.info("Download complete");
+		client.close();
+		return;
 	}
-	core.info("Download complete");
-	client.close();
 }
 
 async function identifyBranch() {
-	if (!/v\d+.\d+.\d+/.test(version)) {
-		throw new Error(
-			`Version string does not fit required format: '${version}'`
-		);
+	try {
+		if (!/v\d+.\d+.\d+/.test(version)) {
+			throw new Error(
+				`Version string does not fit required format: '${version}'`
+			);
+		}
+		if (/alpha/.test(version)) {
+			return "aviatorsEditionCygnus", "alpha";
+		} else if (/beta/.test(version)) {
+			return "aviatorsEditionCygnus", "beta";
+		} else if (/libra/.test(version)) {
+			return "aviatorsEditionLibra", "libra";
+		} else if (!/-/.test(version)) {
+			return "aviatorsEditionLive", "stable";
+		} else if (/releaseCandidate/.test(version)) {
+			return "aviatorsEditionLive", "releaseCandidate";
+		}
+	} catch (err) {
+		throw new Error("Branch unknown");
 	}
-	if (/alpha/.test(version)) {
-		return "aviatorsEditionCygnus", "alpha";
-	} else if (/beta/.test(version)) {
-		return "aviatorsEditionCygnus", "beta";
-	} else if (/libra/.test(version)) {
-		return "aviatorsEditionLibra", "libra";
-	} else if (!/-/.test(version)) {
-		return "aviatorsEditionLive", "stable";
-	} else if (/releaseCandidate/.test(version)) {
-		return "aviatorsEditionLive", "releaseCandidate";
-	}
-	throw new Error("Branch unknown");
 }
 
 async function edit(branch, branchType) {
 	core.info(`Reading File ${fileName}`);
-	var file = fs.readFileSync(fileName);
-	var content = JSON.parse(file);
-	core.info(content);
-	content[branch] = {
-		name: branch,
-		type: branchType,
-		version: version,
-		dateTime: Math.floor(Date.now() / 1000),
-		enabled: true,
-	};
-	fs.writeFileSync(`output-${fileName}`, JSON.stringify(content));
-	core.info("Written");
-	return;
+	try {
+		var file = fs.readFileSync(fileName);
+		var content = JSON.parse(file);
+		core.info(content);
+		content[branch] = {
+			name: branch,
+			type: branchType,
+			version: version,
+			dateTime: Math.floor(Date.now() / 1000),
+			enabled: true,
+		};
+		fs.writeFileSync(`output-${fileName}`, JSON.stringify(content));
+	} catch (err) {
+		throw new Error(error);
+	} finally {
+		core.info("Written");
+		return;
+	}
 }
 
 async function run() {
-	let branch,
-		branchType = await identifyBranch();
-	core.info(`Branch is '${branch}'; type is: '${branchType}'`);
-	await download();
-	await edit(branch, branchType);
-	await upload();
+	try {
+		let branch,
+			branchType = await identifyBranch();
+		core.info(`Branch is '${branch}'; type is: '${branchType}'`);
+		await download();
+		await edit(branch, branchType);
+		await upload();
+	} catch (err) {
+		core.error(`An error occurred in updating the updater listing`);
+		console.error(err);
+	}
 }
 run();
